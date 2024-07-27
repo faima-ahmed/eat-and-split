c2b722760c77420de9d08b463d4e7e4379d0a25d
faima-ahmed <faimaahmeddiya@gmail.com>
2024-07-27 10:16:12 +0600

    Commit 1

--- src/App.js
+++ src/App.js
@@ -145,12 +145,18 @@ function FormAddFriend({onAddFriend}){
 }
 
 function FormSplitBill({selectedFriend}) {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] =useState("");
+  const [whoIsPaying, setWhoIsPaying] =useState("user"); 
+   
   return(
     <form className="form-split-bill">
       <h2>Split a bill with {selectedFriend.name}</h2>
 
       <label>💰 Bill value</label>
-      <input type="text"/>
+      <input type="text"
+      value={bill}
+      onChange={(e) => setBill(e.target.value)}/>
 
       <label>🧍‍♀️ Your expense</label>
       <input type="text"/>
